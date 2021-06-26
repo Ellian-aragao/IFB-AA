a4aebd197fa164627aefc97d3592f53e367f351f
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-06-26 14:17:14 -0300

    inicio de prototipação da main

--- projeto1AnaliseMetodosOrdenacao/main.c
+++ projeto1AnaliseMetodosOrdenacao/main.c
@@ -1,4 +1,6 @@
 #include "config.h"
+#include "sort.h"
+
 
 int main(int argc, char const *argv[])
 {
@@ -7,8 +9,13 @@ int main(int argc, char const *argv[])
     perror("Deve-se enviar valores de entrada no programa");
     exit(EXIT_FAILURE);
   }
-  FILE *fp = fopen(argv[1], "rb");
-  
-
+  FILE *read = fopen(FILENAME_NUMEROS_RANDOMICOS, "rb");
+  int buf;
+  for (size_t i = 0; i < QUANTIDADE_DE_NUMEROS; i++)
+  {
+    fread(&buf, sizeof(int), 1, read);
+    printf("%d\n", buf);
+  }
+  fclose(read);
   return 0;
 }
