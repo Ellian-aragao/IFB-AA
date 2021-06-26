cab0f768152ab458861c8ebfc040e6b47b609060
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-06-26 11:49:55 -0300

    arquivo de configuração do sistema

---
+++ projeto1AnaliseMetodosOrdenacao/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define QUANTIDADE_DE_NUMEROS 1e9
+#define FILENAME_NUMEROS_CRESCENTES "numeros_crescentes.bin"
+#define FILENAME_NUMEROS_DECRESCENTES "numeros_decrescentes.bin"
+#define FILENAME_NUMEROS_RANDOMICOS "numeros_randomicos.bin"
+
+#endif // !CONFIG_H
