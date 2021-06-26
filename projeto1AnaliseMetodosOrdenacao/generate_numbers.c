a7112abbdf22c89e51d7f59e96f4a56977fc1d17
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-06-26 14:15:53 -0300

    geração de função para mostragem de erro e verificação do arquivo de configuração

--- projeto1AnaliseMetodosOrdenacao/generate_numbers.c
+++ projeto1AnaliseMetodosOrdenacao/generate_numbers.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #include "config.h"
+
+static inline void error_exit(const char *error_msg)
+{
+  perror(error_msg);
+  exit(EXIT_FAILURE);
+}
 
 void create_bin_file(const char *file_name, void (*func)(FILE *))
 {
   FILE *file;
-  printf("Criando arquivo %s\n", file_name);
+  const char *divisor = "-------------------------------------------------\n";
+  printf("%sCriando arquivo: %s\n", divisor, file_name);
   if ((file = fopen(file_name, "wb")) == NULL)
-  {
-    perror("Erro ao criar arquivo");
-    exit(EXIT_FAILURE);
-  }
-  puts("Arquivo criado com sucesso\nPopulando dados no arquivo");
+    error_exit("Erro ao criar arquivo");
+  puts("Arquivo criado com SUCESSO\nPopulando dados no arquivo");
   func(file);
   puts("Processo concluído com sucesso");
   fclose(file);
@@ -42,17 +47,17 @@ void generate_numbers_random(FILE *file)
 
 void verify_configs()
 {
-  int
-  if (MAX_NUMEROS <= 0)
-  
-  FILENAME_NUMEROS_CRESCENTES
-  FILENAME_NUMEROS_DECRESCENTES
-  FILENAME_NUMEROS_RANDOMICOS
+  puts("Analizando arquivo de configuração");
+  if (QUANTIDADE_DE_NUMEROS <= 0)
+    error_exit("quantidade de numeros não pode ser menor que 1");
+  const char nome_invalido[] = "Nome do arquivo não pode ser vazio";
+  if (!strlen(FILENAME_NUMEROS_CRESCENTES) || !strlen(FILENAME_NUMEROS_DECRESCENTES) || !strlen(FILENAME_NUMEROS_RANDOMICOS))
+    error_exit(nome_invalido);
 }
 
 int main()
 {
-
+  verify_configs();
   create_bin_file(FILENAME_NUMEROS_CRESCENTES, generate_numbers_crescente);
   create_bin_file(FILENAME_NUMEROS_DECRESCENTES, generate_numbers_decrescente);
   create_bin_file(FILENAME_NUMEROS_RANDOMICOS, generate_numbers_random);
