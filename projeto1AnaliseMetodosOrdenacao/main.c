cf78a33a3b8cdb84e2bd7e5469d76785241e678d
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-06-28 21:48:44 -0300

    processamento de arquivo dinâmico e melhor nomeção de erro

--- projeto1AnaliseMetodosOrdenacao/main.c
+++ projeto1AnaliseMetodosOrdenacao/main.c
@@ -1,5 +1,5 @@
 #include "config.h"
-#include "sort.h"
+#include "sorts/sort.h"
 
 void salve_read_file_data_in_vector(int *vector, const char *file_name);
 int compare_integer(void *i1, void *i2);
@@ -9,15 +9,21 @@ int main(int argc, char const *argv[])
 {
   if (argc < 2)
   {
-    perror("Deve-se enviar valores de entrada no programa");
+    perror("Deve-se enviar um arquivo de entrada no programa");
     exit(EXIT_FAILURE);
   }
+
   int *vector;
   if ((vector = (int *)malloc((sizeof(int)) * QUANTIDADE_DE_NUMEROS)) == NULL)
   {
     perror("Erro o alocar memória para vetor de ordenação");
     exit(EXIT_FAILURE);
   }
+
+  salve_read_file_data_in_vector(vector, argv[1]);
+  bubble_sort(vector, QUANTIDADE_DE_NUMEROS, sizeof(int), compare_integer);
+  free(vector);
+  return EXIT_SUCCESS;
 }
 
 void salve_read_file_data_in_vector(int *vector, const char *file_name)
@@ -28,10 +34,8 @@ void salve_read_file_data_in_vector(int *vector, const char *file_name)
     fprintf(stderr, "Erro ao ler o arquivo %s\n", file_name);
     exit(EXIT_FAILURE);
   }
-
   for (size_t i = 0; i < QUANTIDADE_DE_NUMEROS; i++)
     fread(&vector[i], sizeof(int), 1, read);
-  fclose(read);
 }
 
 void print_vector(const int *vector, const int tamVector)
