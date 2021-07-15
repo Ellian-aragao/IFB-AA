45be399d96f13274957a619f28d3a5637e5058a1
Ellian Aragão Dias <ellian.aragao@gmail.com>
2021-07-15 19:35:23 -0300

    python - troca de função da api de processo

--- projeto1AnaliseMetodosOrdenacao/execute.py
+++ projeto1AnaliseMetodosOrdenacao/execute.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 
-from subprocess import call, Popen
+from subprocess import run, Popen
 from os import listdir, replace
 from os.path import isdir
 from typing import List
@@ -11,7 +11,7 @@ def verify_generated_numbers_files(listFiles: List[str]) -> List[str]:
     if len(listFiles) > 2:
         return filterendwith_mappath_listfiles('dist', listFiles, '.bin')
     print('Iniciando programa de geração de números para sort')
-    call(['dist/generate_numbers'])
+    run(['dist/generate_numbers'])
     listBinarys = []
     for file in filter(lambda file: file.endswith('.bin'), listdir()):
         filePath = 'dist/{}'.format(file)
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     listFiles: List[str] = listdir('dist')
     binArray = verify_generated_numbers_files(listFiles)
     algArray = ['b', 'm', 'h']
-    entradas = [1,10,1e2,1e3,1e4,1e5,1e6,1e7,1e8,1e9]
+    entradas = [1e0,1e1,1e2,1e3,1e4,1e5,1e6,1e7,1e8,1e9]
     for file in binArray:
         for alg in algArray:
             for entrada in entradas:
                 cmd = ['./memusg.py', 'dist/sorts', alg, file, 'b', str(entrada)]
                 print('cmd: {}'.format(cmd), end='')
-                call(cmd)
+                run(cmd)
