4047563d47185fbb06d8f4c6dd6661c79c5d4688
vitorsr116 <vitorsr116@gmail.com>
2024-08-14 23:06:30 -0300

    implementação das coleções e do executeSort

--- 2024/projeto2-analise-algoritmo-sqrsort/src/main/java/ellian/aragao/github/algoritmo/SqrSort.java
+++ 2024/projeto2-analise-algoritmo-sqrsort/src/main/java/ellian/aragao/github/algoritmo/SqrSort.java
@@ -17,9 +17,36 @@ public abstract class SqrSort extends BaseValidation {
         final var intSize = (int) sqrtSizeCollection;
         final var modSize = sizeCollection % intSize;
         final var finalCollection = new ArrayList<E>(sizeCollection);
+        final var collectionOfCollection = new ArrayList<List <E>>( intSize + 1);
+        for (int i = 0; i <  intSize; i++) {
+            final var prefixIndex = i * intSize;
+            final var subListaOrdenada = executeSort(collection.subList(prefixIndex, prefixIndex + intSize));
+            collectionOfCollection.add(subListaOrdenada);
+        }
+        collectionOfCollection.add(collection.subList(intSize * (intSize - 1) , sizeCollection - 1));
+
+        while (!collectionOfCollection.isEmpty() ){
+            for (var subListaOrdenada: collectionOfCollection) {
+                if (subListaOrdenada.isEmpty()){
+                    collectionOfCollection.remove(subListaOrdenada);
+                    break;
+                }
+
+
+                final var maior = subListaOrdenada.removeLast();
+                final var maiorComparacao = maior.compareTo(finalCollection.getLast());
+
+                if (maiorComparacao > 0) {
+                    finalCollection.add(maior);
+                }
+            }
+
+        }
 
 
 
         return finalCollection;
     }
+
+    protected abstract <E extends Comparable<E>> List<E> executeSort(List<E> es);
 }
