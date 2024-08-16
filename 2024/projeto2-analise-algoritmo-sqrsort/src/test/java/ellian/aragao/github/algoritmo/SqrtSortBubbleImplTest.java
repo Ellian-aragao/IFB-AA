386c2711f7cc42f55e6f8f4645671ac4c366c1c9
vitorsr116 <vitorsr116@gmail.com>
2024-08-16 09:59:42 -0300

    test: Implementação tanto do SortHeap e de uma ordem maior decrescente

--- 2024/projeto2-analise-algoritmo-sqrsort/src/test/java/ellian/aragao/github/algoritmo/SqrtSortBubbleImplTest.java
+++ 2024/projeto2-analise-algoritmo-sqrsort/src/test/java/ellian/aragao/github/algoritmo/SqrtSortBubbleImplTest.java
@@ -3,6 +3,7 @@ package ellian.aragao.github.algoritmo;
 import ellian.aragao.github.utils.GeradorDeNumeros;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,6 +15,14 @@ class SqrtSortBubbleImplTest {
 
     @Test
     void listaDecrescenteSqrtSortBubble() {
+
+        List<Long> listDesc = geradorDeNumeros.inicializaListaDecrescente(9);
+        final var result = sqrtSortBubbleImpl.executeSort(listDesc);
+        assertEquals(List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L), result);
+    }
+
+    @Test
+    void listaDecrescenteMaiorSqrtSortBubble() {
 
         final var result = sqrtSortBubbleImpl.executeSort(geradorDeNumeros.inicializaListaDecrescente(10));
         assertEquals(List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L), result);
